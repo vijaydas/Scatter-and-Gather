956e3305715606f5e2592ab62fd9a2fd20f13eab
Vijay Das <vijaytdas@gmail.com>
2019-02-06 15:41:08 -0600

    add base project

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Vijay Das on 2/6/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
